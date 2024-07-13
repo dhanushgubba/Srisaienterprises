5058c894ccb11618d971d7da8aadc25f58e65020
dhanushgubba.ap@gmail.com <dhanushgubba.ap@gmail.com>
2024-07-13 13:56:30 +0530

    commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,5 @@
+import data from './data';
+
 function App() {
   return (
     <div className="App">
@@ -5,7 +7,23 @@ function App() {
         <a href="/">Sri Sai Enterprises</a>
       </header>
       <main>
-        list of products
+        <h1>Featured Products</h1>
+        <div className="products">
+          {data.products.map(product => (
+            <div className="product" key={product.slug}>
+              <a href={`/product/${product.slug}`}>
+                <img src={product.image} alt={product.name} />
+              </a>
+              <div className="product-info">
+              <a href={`/product/${product.slug}`}>
+                <p>{product.name}</p>
+              </a>
+              <p><strong>${product.price}</strong></p>
+              <button>Add to Cart</button>
+              </div>
+            </div>
+          ))}
+        </div>
       </main>
     </div>
   );
